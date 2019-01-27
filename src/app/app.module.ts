b129cf0993eccd6e76f5184d29ae2f3cf3ae81da
Vinh <nguyenthevinh1997@gmail.com>
2019-01-27 21:38:20 +0700

    Import bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopSidebarComponent } from './top-sidebar/top-sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    TopSidebarComponent,
   ],
   imports: [
     BrowserModule,
