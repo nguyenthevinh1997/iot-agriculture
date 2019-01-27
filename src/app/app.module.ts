4193db5072218af816ccbd430fc2bf568cbaa6d1
Vinh <nguyenthevinh1997@gmail.com>
2019-01-27 21:57:38 +0700

    Import Material Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,57 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TopSidebarComponent } from './top-sidebar/top-sidebar.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { TopSidebarComponent } from "./top-sidebar/top-sidebar.component";
+
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import 'hammerjs';
+import {
+  MatSelectModule,
+  MatSlideToggleModule,
+  MatAutocompleteModule,
+  MatProgressBarModule,
+  MatDialogModule,
+  MatIconModule,
+  MatButtonModule,
+  MatCardModule,
+  MatInputModule,
+  MatListModule,
+  MatSliderModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatMenuModule,
+  MatSnackBarModule,
+  MatTabsModule,
+  MatProgressSpinnerModule
+} from "@angular/material";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TopSidebarComponent,
-  ],
+  declarations: [AppComponent, TopSidebarComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatAutocompleteModule,
+    MatProgressBarModule,
+    MatDialogModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatListModule,
+    MatSliderModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatMenuModule,
+    MatSnackBarModule,
+    MatTabsModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
