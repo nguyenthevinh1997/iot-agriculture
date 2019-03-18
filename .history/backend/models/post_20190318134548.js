901b2b040f1303e3d376fbff29b8178255f53e1c
Vinh <nguyenthevinh1997@gmail.com>
2019-03-18 14:17:57 +0700

    Fix error his

--- .history/backend/models/post_20190318134548.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const postSchema = mongoose.Schema({
-  title: { type: String, require: true },
-  content: { type: String, require: true }
-});
-
-mongoose.model('Post', postSchema);
--- backend/models/post.js
+++ backend/models/post.js
@@ -4,5 +4,5 @@ const postSchema = mongoose.Schema({
   title: { type: String, require: true },
   content: { type: String, require: true }
 });
-
+//
 mongoose.model('Post', postSchema);
