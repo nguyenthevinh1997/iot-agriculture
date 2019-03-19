f732e4d67c7ca5c61045dabc48c922e14d4ed036
Vinh <nguyenthevinh1997@gmail.com>
2019-03-19 09:08:12 +0700

    Fetching data from a database

--- backend/app.js
+++ backend/app.js
@@ -39,23 +39,24 @@ app.post("/api/posts", (req, res, next) => {
   });
 });
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "dasdklj455jd",
-      title: "First Server Side",
-      content: "This is comming from the server!"
-    },
-    {
-      id: "45dsflj455jd",
-      title: "Second Server Side",
-      content: "This is comming from the server!"
-    },
-    {
-      id: "dafdsf6455jd",
-      title: "Third Server Side",
-      content: "This is comming from the server!"
-    }
-  ];
+  // const posts = [
+  //   {
+  //     id: "dasdklj455jd",
+  //     title: "First Server Side",
+  //     content: "This is comming from the server!"
+  //   },
+  //   {
+  //     id: "45dsflj455jd",
+  //     title: "Second Server Side",
+  //     content: "This is comming from the server!"
+  //   },
+  //   {
+  //     id: "dafdsf6455jd",
+  //     title: "Third Server Side",
+  //     content: "This is comming from the server!"
+  //   }
+  // ];
+  
   res.status(200).json({
     message: "Posts fetched successfully",
     posts: posts
