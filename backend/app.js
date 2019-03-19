7329f7f0877a356bf3e7c13f3f6e13b0633044d7
Vinh <nguyenthevinh1997@gmail.com>
2019-03-19 10:57:52 +0700

    MongoDB: 16. Adding Posts with an ID: refreshing after adding post

--- backend/app.js
+++ backend/app.js
@@ -35,10 +35,11 @@ app.post("/api/posts", (req, res, next) => {
     title: req.body.title,
     content: req.body.content
   });
-  post.save();
-  console.log(post);
-  res.status(201).json({
-    message: "Post added successfully"
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: "Post added successfully",
+      postId: createdPost._id
+    });
   });
 });
 app.get("/api/posts", (req, res, next) => {
@@ -52,9 +53,9 @@ app.get("/api/posts", (req, res, next) => {
 });
 app.delete("/api/posts/:id", (req, res, next) => {
   console.log(req.params.id);
-  Post.deleteOne({_id: req.params.id}).then(result=>{
+  Post.deleteOne({ _id: req.params.id }).then(result => {
     console.log(result);
-  })
+  });
   res.status(200).json({
     message: "Posts deleted!"
   });
